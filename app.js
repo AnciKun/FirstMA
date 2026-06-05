08deb3e58605eab3b9279e27474c1c490ce3b12d
Anci <158807593+AnciKun@users.noreply.github.com>
2026-06-05 16:37:53 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 let tg = window.Telegram.WebApp;
 tg.ready();
 tg.expand();
+
+let notifybutton = document.getElementById("open_button");
+
+notifybutton.addEventListener("click", () =>{
+    tg.sendData("PONG");
+    tg.HapticFeedback.impactOccurred('light');
+});
